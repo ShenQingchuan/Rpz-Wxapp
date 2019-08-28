7ef8f4f425a9fd62ad90e98762f3ef9590b7d3eb
ShenQingchuan <tmy1106@outlook.com>
2019-08-28 20:48:49 +0800

    Real name Input to backend mongodb function finished
    now if the officer sign in without his/her real-name info(storage) then the wx_app will show a mask on the index page.
    he/she must submit his/her truename in order to unlock the app.

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,6 +4,13 @@ const app = getApp()
 
 Page({
   data: {
+    truename: '',
+    truenameRules: [{
+      required: true,
+      type: 'string',
+      min: 2,max: 5,message: "长度在2-5个汉字之间"}],
+    inMask: true,
+
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
@@ -38,7 +45,7 @@ Page({
     }
   },
 
-	//路由跳转函数
+	//工作日程路由跳转函数
 	goCalendarPage(){
 		wx.navigateTo({
 			url: '/pages/calendar/calendar',
@@ -53,6 +60,10 @@ Page({
 			success: res=>{
 				console.log("INFO - session_key 有效...");
 				this.getOpenID();
+        // 如果存储过实名制信息，则可以去掉遮罩了
+        if (wx.getStorageSync('truename').length != 0) {
+          this.setData({ inMask: false });
+        }
 			},
 			fail: err=>{
 				console.log("ERROR - session key 过期...");
@@ -81,10 +92,20 @@ Page({
 							code: res.code
 						},
 						success: response => {
-							let openid = response.data.bundle_data.openid;
+							let openid = response.data.bundle_data.wx_bundle.openid;
+              if (response.data.bundle_data.officer_status) {
+                // 如果 后端mongodb中该干事实名信息已存在
+                this.setData({
+                  inMask: !response.data.bundle_data.officer_status
+                }); // 取消遮罩
+                wx.setStorageSync('truename', 'any:yes');
+              }
+              
 							//可以把openid存到本地缓存，方便以后调用
 							wx.setStorageSync('openid', openid);
+
 							app.globalData.user_openid = openid;
+              console.log('getOpenID 执行成功...');
 						},
 						fail: error => {
 							console.log('[ERROR] - openid 解析失败...');
@@ -92,10 +113,82 @@ Page({
 					});
 				}
 			});
-		}
-		else {
+		} else {
 			app.globalData.user_openid = wx.getStorageSync('openid');
 		}
 	},
+
+  // 在用户没有给后台提供本openid的真实姓名前 点击遮罩而触发的绑定事件：
+  showMaskToast() {
+    wx.lin.showToast({
+      title: '请先告诉我你的真实姓名~',
+      icon: 'error',
+      iconStyle: 'color:orange; size: 60',
+    })
+  },
+
+  // 正在输入真实姓名的监听事件：
+  onInputTrueName(e) {
+    console.log(e);
+    this.setData({
+      truename: e.detail.detail.value
+    });
+  },
+
+  /**
+	 * 输入真实姓名结果校验的方法：
+	 */
+  validate(e) {
+    if (e.detail.isError) {
+      for (let i = 0; i < e.detail.errors.length; i++) {
+        wx.lin.showToast({
+          title: e.detail.errors[i].message,
+          icon: 'error',
+          iconStyle: 'color: red; size: 60',
+        });
+      }
+      this.setData({
+        truename: ''
+      });
+    }
+  },
+
+  // 提交真实姓名按钮点击事件：
+  submitTrueName() {
+    let openid = wx.getStorageSync('openid')
+    wx.request({
+      url: 'http://localhost:9090/v1/weixin/initOfficer',
+      method: 'POST',
+      data: {
+        "openid": openid,
+        "truename": this.data.truename
+      },
+      success: response => {
+        //后台创建本干事实名信息成功，撤下 mask
+        if(response.statusCode !== 406) {
+          this.setData({
+            inMask: false
+          });
+          wx.setStorage({
+            key: 'truename',
+            data: this.data.truename
+          });
+        } else {
+          wx.lin.showToast({
+            title: '创建实名信息请求失败！',
+            icon: 'error',
+            iconStyle: 'color: red; size: 60',
+          })
+        }
+      },
+      fail: error => {
+        wx.lin.showToast({
+          title: '创建实名信息请求失败！',
+          icon: 'error',
+          iconStyle: 'color: red; size: 60',
+        });
+      }
+    })
+  }
 
 })
